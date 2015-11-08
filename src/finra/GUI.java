484d6b29b8f60322b648e796965fa0a8cd16d455
Ben Greenberg <bengreenberg5@gmail.com>
2015-11-07 22:55:19 -0500

    Add GUI

---
+++ src/finra/GUI.java
@@ -0,0 +1,60 @@
+package finra;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.*;
+
+// see Zoom.java
+public class GUI {
+ 
+	public static void main(String[] args) throws IOException {
+		makeWindow(); 
+		// y, then x
+		ArrayList<ArrayList<Integer>> startPoints = EndpointFinder.startpoints(2000, 2500);
+		System.out.println(startPoints.get(0).toString());
+		System.out.println(startPoints.get(1).toString());
+		photoOutput.createPicture(startPoints);
+	}
+	
+	public static void makeWindow() {
+		try {
+			ImagePanel panel = new ImagePanel();
+	        ImageZoom zoom = new ImageZoom(panel);
+	        JFrame f = new JFrame();
+	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        f.getContentPane().add(zoom.getUIPanel(), "North");
+	        f.getContentPane().add(new JScrollPane(panel));
+	        f.setSize(2400,1561);
+	        f.setLocation(40,40);
+	        f.setVisible(true);
+		} catch (IOException io) {
+			System.out.println("i/o error in getCoords (GUI.java)");
+		}
+	}
+	
+	public static int[] getCoords() {
+		try {
+			File file = new File("coords.txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			Scanner sc = new Scanner(br);
+			int counter = 0, x = -1, y = -1;
+			while (sc.hasNextLine()) {
+				int coord = Integer.parseInt(sc.nextLine());
+				if (counter % 2 == 0) {
+					x = coord;
+				} else {
+					y = coord;
+				}
+				counter++;
+			}
+			sc.close();
+			return new int[] {x, y};
+		} catch (IOException io) {
+			System.out.println("no point selected!");
+		}
+        return new int[] {-1, -1};
+	}
+
+}
---
+++ src/finra/Zoom.java
@@ -0,0 +1,139 @@
+package finra;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.*;
+  
+public class Zoom
+{
+    
+}
+  
+@SuppressWarnings("serial")
+class ImagePanel extends JPanel
+{
+    BufferedImage image;
+    double scale;
+  
+    public ImagePanel() throws IOException
+    {
+        loadImage();
+        scale = 1.0;
+        setBackground(Color.black);
+        connectToMouse();
+    }
+    
+    protected void connectToMouse() {
+    	File f = new File("coords.txt");
+    	try {
+        f.createNewFile();
+    	} catch (IOException io) {
+    		System.out.println("error making new file in Zoom.java");
+    	}
+    	this.addMouseListener(new MouseInputAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				try { 
+				BufferedWriter coordWriter = new BufferedWriter(new FileWriter(f));
+				int x = e.getX();
+				int y = e.getY();
+				coordWriter.write(Integer.toString(x));
+				coordWriter.newLine();
+				coordWriter.write(Integer.toString(y));
+				coordWriter.close();
+				System.out.println(x + ", " + y);
+				} catch (IOException io) {
+					System.out.println("error connecting MouseListener in Zoom.java");
+				}
+			}
+		});
+    }
+  
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                            RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        int w = getWidth();
+        int h = getHeight();
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+        double x = (w - scale * imageWidth)/2;
+        double y = (h - scale * imageHeight)/2;
+        AffineTransform at = AffineTransform.getTranslateInstance(x,y);
+        at.scale(scale, scale);
+        g2.drawRenderedImage(image, at);
+    }
+  
+    /**
+     * For the scroll pane.
+     */
+    public Dimension getPreferredSize()
+    {
+        int w = (int)(scale * image.getWidth());
+        int h = (int)(scale * image.getHeight());
+        return new Dimension(w, h);
+    }
+  
+    public void setScale(double s)
+    {
+        scale = s;
+        revalidate();      // update the scroll pane
+        repaint();
+    }
+  
+    private void loadImage()
+    {
+        String fileName = "images/NYC_portrait.jpg";
+        try
+        {
+            image = ImageIO.read(new File (fileName));
+        }
+        catch(MalformedURLException mue)
+        {
+            System.out.println("URL trouble: " + mue.getMessage());
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("read trouble: " + ioe.getMessage());
+        }
+    }
+}
+  
+class ImageZoom
+{
+    ImagePanel imagePanel;
+  
+    public ImageZoom(ImagePanel ip)
+    {
+        imagePanel = ip;
+    }
+  
+    public JPanel getUIPanel()
+    {
+    	// default, min, max, incr
+        SpinnerNumberModel model = new SpinnerNumberModel(1.0, 0.1, 4.0, 0.15);
+        final JSpinner spinner = new JSpinner(model);
+        spinner.setPreferredSize(new Dimension(60, spinner.getPreferredSize().height));
+        spinner.addChangeListener(new ChangeListener()
+        {
+            public void stateChanged(ChangeEvent e)
+            {
+                float scale = ((Double)spinner.getValue()).floatValue();
+                imagePanel.setScale(scale);
+            }
+        });
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("scale");
+        panel.add(label);
+        panel.add(spinner);
+        return panel;
+    }
+}
